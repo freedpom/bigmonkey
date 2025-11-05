509053f28de7512e397d3087f85dacdcfa8c9717
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-11-05 12:24:48 -0500

    feat(disko): auto generation of pool devices

--- hosts/bigmonkey/disko.nix
+++ hosts/bigmonkey/disko.nix
@@ -1,116 +1,134 @@
 {
-  disko.devices = {
-    disk = {
-      nix = {
-        type = "disk";
-        device = "/dev/disk/by-id/nvme-WD_BLACK_SN770_1TB_234252800502";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = ["umask=0077"];
-              };
-            };
+  disko.devices = let
+    getIdFromDevice = device: let
+      parts = builtins.split "/" device;
+      devName = builtins.elemAt parts (builtins.length parts - 1);
+      len = builtins.stringLength devName;
+    in
+      builtins.substring (len - 4) 4 devName;
 
-            root = {
-              size = "100%";
-              content = {
-                type = "bcachefs";
-                filesystem = "root";
-                label = "root";
-                extraFormatArgs = ["--discard"];
+    bcachefsCommon = {
+      filesystem,
+      extraFormatArgs ? [],
+      ...
+    }: {
+      type = "bcachefs";
+      inherit filesystem extraFormatArgs;
+    };
+
+    fastpoolArgs = [
+      "--discard"
+      "--compression=zstd"
+      "--background_compression=zstd"
+      "--data_replicas=1"
+      "--metadata_replicas=2"
+    ];
+
+    bulkpoolArgs = [
+      "--discard"
+      "--compression=zstd"
+      "--background_compression=zstd"
+      "--replicas=2"
+    ];
+
+    mkDisk = {
+      device,
+      pool,
+      args,
+    }: let
+      id = getIdFromDevice device;
+    in {
+      type = "disk";
+      inherit device;
+      content =
+        (bcachefsCommon {
+          filesystem = pool;
+          extraFormatArgs = args;
+        })
+        // {
+          label = "${pool}_${id}";
+        };
+    };
+
+    fastpoolDevices = [
+      "/dev/disk/by-id/nvme-CT2000P310SSD8_24514CF66749"
+      "/dev/disk/by-id/nvme-CT2000P310SSD8_24514CF67818"
+      "/dev/disk/by-id/nvme-CT2000P310SSD8_24514CF67847"
+      "/dev/disk/by-id/nvme-CT2000P310SSD8_24514CF67917"
+    ];
+
+    bulkpoolDevices = [
+      "/dev/disk/by-id/ata-ST4000VN006-3CW104_ZW603ALH"
+      "/dev/disk/by-id/ata-ST4000VN006-3CW104_ZW6040NG"
+    ];
+
+    fastpoolDiskMap = builtins.listToAttrs (
+      map (dev: {
+        name = "nvme-${getIdFromDevice dev}";
+        value = mkDisk {
+          device = dev;
+          pool = "fastpool";
+          args = fastpoolArgs;
+        };
+      })
+      fastpoolDevices
+    );
+
+    bulkpoolDiskMap = builtins.listToAttrs (
+      map (dev: {
+        name = "hdd-${getIdFromDevice dev}";
+        value = mkDisk {
+          device = dev;
+          pool = "bulkpool";
+          args = bulkpoolArgs;
+        };
+      })
+      bulkpoolDevices
+    );
+  in {
+    disk =
+      fastpoolDiskMap
+      // bulkpoolDiskMap
+      // {
+        nix = {
+          type = "disk";
+          device = "/dev/disk/by-id/nvme-WD_BLACK_SN770_1TB_234252800502";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                size = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = ["umask=0077"];
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "bcachefs";
+                  filesystem = "rootfs";
+                  label = "root";
+                  extraFormatArgs = ["--discard"];
+                };
               };
             };
           };
         };
       };
 
-      # NVME raid drives
-      nvme-6749 = {
-        type = "disk";
-        device = "/dev/disk/by-id/nvme-CT2000P310SSD8_24514CF66749";
-        content = {
-          type = "bcachefs";
-          filesystem = "np1";
-          label = "np1.6749";
-          extraFormatArgs = ["--discard"];
-        };
-      };
-
-      nvme-7818 = {
-        type = "disk";
-        device = "/dev/disk/by-id/nvme-CT2000P310SSD8_24514CF67818";
-        content = {
-          type = "bcachefs";
-          filesystem = "np1";
-          label = "np1.7818";
-          extraFormatArgs = ["--discard"];
-        };
-      };
-
-      nvme-7847 = {
-        type = "disk";
-        device = "/dev/disk/by-id/nvme-CT2000P310SSD8_24514CF67847";
-        content = {
-          type = "bcachefs";
-          filesystem = "np1";
-          label = "np1.7847";
-          extraFormatArgs = ["--discard"];
-        };
-      };
-
-      nvme-7917 = {
-        type = "disk";
-        device = "/dev/disk/by-id/nvme-CT2000P310SSD8_24514CF67917";
-        content = {
-          type = "bcachefs";
-          filesystem = "np1";
-          label = "np1.7917";
-          extraFormatArgs = ["--discard"];
-        };
-      };
-
-      # HDD raid drives
-      hdd-3ALH = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-ST4000VN006-3CW104_ZW603ALH";
-        content = {
-          type = "bcachefs";
-          filesystem = "dp1";
-          label = "dp1.3ALH";
-          extraFormatArgs = ["--discard"];
-        };
-      };
-
-      hdd-40NG = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-ST4000VN006-3CW104_ZW6040NG";
-        content = {
-          type = "bcachefs";
-          filesystem = "dp1";
-          label = "dp1.40NG";
-          extraFormatArgs = ["--discard"];
-        };
-      };
-    };
-
     bcachefs_filesystems = {
-      root = {
+      rootfs = {
         type = "bcachefs_filesystem";
-        extraFormatArgs = [
-          "--compression zstd:1"
-        ];
+        extraFormatArgs = ["--compression=zstd"];
         subvolumes = {
           nix = {
             mountpoint = "/nix";
           };
-          root = {
+          persist = {
             mountpoint = "/nix/persist";
           };
           home = {
@@ -119,34 +137,16 @@
         };
       };
 
-      np1 = {
+      fastpool = {
         type = "bcachefs_filesystem";
-        extraFormatArgs = [
-          "--compression=zstd"
-          "--background_compression=ztsd"
-          #"--erasure_code" Not stable yet according to manpage
-          "--data_replicas=1"
-          "--metadata_replicas=2"
-        ];
-        subvolumes = {
-          root = {
-            mountpoint = "/nix/persist/npool";
-          };
-        };
+        extraFormatArgs = fastpoolArgs;
+        subvolumes.root.mountpoint = "/nix/persist/fastpool";
       };
 
-      dp1 = {
+      bulkpool = {
         type = "bcachefs_filesystem";
-        extraFormatArgs = [
-          "--compression=zstd"
-          "--background_compression=zstd"
-          "--replicas=2"
-        ];
-        subvolumes = {
-          root = {
-            mountpoint = "/nix/persist/dpool";
-          };
-        };
+        extraFormatArgs = bulkpoolArgs;
+        subvolumes.root.mountpoint = "/nix/persist/bulkpool";
       };
     };
 
