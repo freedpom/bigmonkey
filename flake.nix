67634d503e38cd91b825ff84413b85acc31ec6ec
yung-gimp <172232649+yung-gimp@users.noreply.github.com>
2025-11-29 23:52:31 -0500

    funnie inputs

--- flake.nix
+++ flake.nix
@@ -13,7 +13,6 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
     ];
-    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
   };
 
   outputs =
@@ -65,5 +64,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     preservation.url = "github:nix-community/preservation";
+
+    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
   };
 }
